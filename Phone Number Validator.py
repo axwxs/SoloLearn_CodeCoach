6819adef129c7cc5069855ea3aa8046a6e89d995
axwxs <72666513+axwxs@users.noreply.github.com>
2021-05-10 01:55:14 -0400

    Create Phone Number Validator.py

---
+++ Phone Number Validator.py
@@ -0,0 +1,34 @@
+'''
+You are given a number input, and need to check if it is a valid phone number.
+A valid phone number has exactly 8 digits and starts with 1, 8 or 9. 
+Output "Valid" if the number is valid and "Invalid", if it is not. 
+
+Sample Input
+81239870
+
+Sample Output 
+Valid
+'''
+
+import re
+#your code goes here
+
+x = input()
+
+pattern = r"^(9|8|1)+(\d+\d+\d+\d+\d+\d+\d)$"
+
+match = re.match(pattern, x)
+
+
+if match:
+	if(len(x)) == 8:
+		print('Valid')
+	else:
+		print('Invalid')
+else:
+	print('Invalid')
+
+
+
+
+
